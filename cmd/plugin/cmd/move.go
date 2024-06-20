88722aa7d23b1ded43464d8a7f07671c7ec9633d
Will Foster <wfoster@newrelic.com>
2024-06-19 22:24:05 -0700

    Implement "move" support

--- cmd/plugin/cmd/move.go
+++ cmd/plugin/cmd/move.go
@@ -18,9 +18,12 @@ package cmd
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/go-errors/errors"
 	"github.com/spf13/cobra"
+	"os"
+	"sigs.k8s.io/cluster-api/cmd/clusterctl/client/cluster"
+	configclient "sigs.k8s.io/cluster-api/cmd/clusterctl/client/config"
 )
 
 type moveOptions struct {
@@ -100,5 +103,111 @@ func runMove() error {
 }
 
 func moveProvider(ctx context.Context, opts *moveOptions) error {
-	return errors.New("Not implemented")
+	// Both backup and restore makes no sense. It's a complete move.
+	if opts.fromDirectory != "" && opts.toDirectory != "" {
+		return errors.Errorf("can't set both FromDirectory and ToDirectory")
+	}
+
+	if !opts.dryRun &&
+		opts.fromDirectory == "" &&
+		opts.toDirectory == "" &&
+		opts.toKubeconfig == "" {
+		return errors.Errorf("at least one of FromDirectory, ToDirectory and ToKubeconfig must be set")
+	}
+
+	if opts.toDirectory != "" {
+		return toDirectory(ctx, opts)
+	} else if opts.fromDirectory != "" {
+		return fromDirectory(ctx, opts)
+	}
+
+	return moveProvider(ctx, opts)
+}
+
+func move(ctx context.Context, opts *moveOptions) error {
+	// Get the client for interacting with the source management cluster.
+	fromCluster, err := getClusterClient(ctx, cluster.Kubeconfig{Path: opts.fromKubeconfig, Context: opts.fromKubeconfigContext})
+	if err != nil {
+		return err
+	}
+
+	// If the option specifying the Namespace is empty, try to detect it.
+	if opts.namespace == "" {
+		currentNamespace, err := fromCluster.Proxy().CurrentNamespace()
+		if err != nil {
+			return err
+		}
+		opts.namespace = currentNamespace
+	}
+
+	var toCluster cluster.Client
+	if !opts.dryRun {
+		// Get the client for interacting with the target management cluster.
+		if toCluster, err = getClusterClient(ctx, cluster.Kubeconfig{Path: opts.toKubeconfig, Context: opts.toKubeconfigContext}); err != nil {
+			return err
+		}
+	}
+
+	return fromCluster.ObjectMover().Move(ctx, opts.namespace, toCluster, opts.dryRun)
+}
+
+func fromDirectory(ctx context.Context, opts *moveOptions) error {
+	toCluster, err := getClusterClient(ctx, cluster.Kubeconfig{Path: opts.toKubeconfig, Context: opts.toKubeconfigContext})
+	if err != nil {
+		return err
+	}
+
+	if _, err := os.Stat(opts.fromDirectory); os.IsNotExist(err) {
+		return err
+	}
+
+	return toCluster.ObjectMover().FromDirectory(ctx, toCluster, opts.fromDirectory)
+}
+
+func toDirectory(ctx context.Context, opts *moveOptions) error {
+	fromCluster, err := getClusterClient(ctx, cluster.Kubeconfig{Path: opts.fromKubeconfig, Context: opts.fromKubeconfigContext})
+	if err != nil {
+		return err
+	}
+
+	// If the option specifying the Namespace is empty, try to detect it.
+	if opts.namespace == "" {
+		currentNamespace, err := fromCluster.Proxy().CurrentNamespace()
+		if err != nil {
+			return err
+		}
+		opts.namespace = currentNamespace
+	}
+
+	if _, err := os.Stat(opts.toDirectory); os.IsNotExist(err) {
+		return err
+	}
+
+	return fromCluster.ObjectMover().ToDirectory(ctx, opts.namespace, opts.toDirectory)
+}
+
+func getClusterClient(ctx context.Context, kubeconfig cluster.Kubeconfig) (cluster.Client, error) {
+	configClient, err := configclient.New(ctx, "")
+	if err != nil {
+		return nil, fmt.Errorf("cannot create config client: %w", err)
+	}
+
+	cluster := cluster.New(
+		kubeconfig,
+		configClient,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Ensure this command only runs against management clusters with the current Cluster API contract.
+	if err := cluster.ProviderInventory().CheckCAPIContract(ctx); err != nil {
+		return nil, err
+	}
+
+	// Ensures the custom resource definitions required by clusterctl are in place.
+	if err := cluster.ProviderInventory().EnsureCustomResourceDefinitions(ctx); err != nil {
+		return nil, err
+	}
+	return cluster, nil
 }
